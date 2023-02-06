e8f7e498234a3b6013a53c61273e69bbecb1c1fd
kavabunga <kavabunga@gmail.com>
2023-02-06 20:30:22 +0200

    fix: hotfix server link

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -39,8 +39,8 @@ const validationConfig = {
   inputErrorClass: 'form__input_type_error'
 }
 
-// const serverUrl = 'https://api.nostromo.nomoredomainsclub.ru'
-const serverUrl = 'http://localhost:3000'
+const serverUrl = 'https://api.nostromo.nomoredomainsclub.ru'
+// const serverUrl = 'http://localhost:3000'
 const authUrl = serverUrl
 
 export {
